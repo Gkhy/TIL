8df651dd685640c4d900ef8bf114a03494a7e3ab
Gkhy <syan1022@naver.com>
2022-07-22 01:24:20 +0900

    c

--- 실습/1926.py
+++ 실습/1926.py
@@ -1,13 +1,10 @@
 import sys
 sys.stdin = open("1926input.txt", "r")
-#3 6 9 나올때마다 -넣어야함 두번은 --이다
-#str으로 접근하면 반복에 용이할것
-#print(end=" ")
-#cnt에 들어가는 숫자만큼 박수
+
 T=int(input())
 S=['3','6','9']
 
-for i in range(T):
+for i in range(1,T+1):
     i=str(i)
     cnt=0
     for i1 in i:
@@ -18,7 +15,6 @@ for i in range(T):
     else:
         print(cnt*'-',end=' ')
    
-i=str(12)
-b=i.split(',')
-print(b)
+
+
 
